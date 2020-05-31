51a3d58577bafe41c78b872551394e4cfe4ab5e8
Steffen <sasjurse@gmail.com>
2020-05-31 10:03:18 +0200

    extra plot snap

--- src/plots.py
+++ src/plots.py
@@ -14,9 +14,16 @@ import pandas as pd
 from generics.postgres import dataframe_from_sql
 from generics.file_locations import plots_folder
 
+
+def append_snap_to_fig(fig):
+    for column in ['snap_CA', 'snap_TX', 'snap_WI']:
+        snap_days = dataframe_from_sql(f'select date, "{column}" from calendar where "{column}"=1')
+        trace_1 = go.Scatter(x=snap_days['date'], y=snap_days[column], name=column, mode='markers', yaxis='y2')
+        fig.add_trace(trace_1)
+
+    return fig
+
 #%%
-
-
 
 sql = 'select date, store_id, sum(quantity) as quantity from sales_by_day group by 1,2 order by date desc'
 df = dataframe_from_sql(sql)
@@ -62,16 +69,40 @@ fig = px.line(df, x="date", y="quantity", color='dept_id')
 
 plotly.offline.plot(fig, filename=str(plots_folder() / 'sales_by_dept_MA.html'))
 
+#%%
+
+
+sql = """
+with base as (
+select 
+    date
+    ,dept_id
+    ,sum(quantity) as quantity 
+from sales_by_day 
+where store_id ='CA_1'
+group by 1,2
+)
+
+select 
+    date
+    ,dept_id
+    ,sum(quantity) over w as quantity 
+from base
+window w as (partition by dept_id order by date desc rows between 3 preceding and 3 following)
+order by 1 desc"""
+
+
+df = dataframe_from_sql(sql)
+
+fig = px.line(df, x="date", y="quantity", color='dept_id')
+fig = append_snap_to_fig((fig))
+
+plotly.offline.plot(fig, filename=str(plots_folder() / 'sales_by_dept_one_store_MA.html'))
+
 
 #%%
 
-def append_snap_to_fig(fig):
-    for column in ['snap_CA', 'snap_TX', 'snap_WI']:
-        snap_days = dataframe_from_sql(f'select date, "{column}" from calendar where "{column}"=1')
-        trace_1 = go.Scatter(x=snap_days['date'], y=snap_days[column], name=column, mode='markers', yaxis='y2')
-        fig.add_trace(trace_1)
 
-    return fig
 
 
 sql = """
